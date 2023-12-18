9320e2cb76319e6c2427c72936933213ff411947
Anton G Neuhold Jr <agneuhold@gmail.com>
2023-12-18 15:48:36 -0800

    Export DocumentDb

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import ApiKeyRepository from './repositories/common/ApiKeyRepository';
 import UserRepository from './repositories/common/UserRepository';
+import DocumentDb from './util/DocumentDb';
 
 // Export all the functions and classes from this library
-export { UserRepository, ApiKeyRepository };
+export { UserRepository, ApiKeyRepository, DocumentDb };
 
 // Export TypeScript types where needed
 export type {};
