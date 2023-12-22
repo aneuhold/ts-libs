c75e0ada8b25bfd98c660c5225acf59fb00e6341
Anton G Neuhold Jr <agneuhold@gmail.com>
2023-12-22 15:34:50 -0800

    Fix issue with Document type

--- src/documents/BaseDocument.ts
+++ src/documents/BaseDocument.ts
@@ -4,6 +4,6 @@ import { ObjectId, Document } from 'bson';
  * A base document which other types that will be stored in a document DB can
  * inherit from.
  */
-export default abstract class BaseDocument extends Document {
+export default abstract class BaseDocument implements Document {
   _id = new ObjectId();
 }
