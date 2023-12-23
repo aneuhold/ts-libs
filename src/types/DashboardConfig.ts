ea614d3ae043ceb25de90569e9311c09b3a940bc
Anton G Neuhold Jr <agneuhold@gmail.com>
2023-12-22 18:36:23 -0800

    Update config type

--- src/types/DashboardConfig.ts
+++ src/types/DashboardConfig.ts
@@ -4,4 +4,11 @@
  */
 export type DashboardConfig = {
   projectDashboardFunctionUrl: string;
+  automationUrls: {
+    sunLight: string;
+    zoomLighting: string;
+    bedTimeButStillWatchingShows: string;
+    bedTime: string;
+    sunrise: string;
+  };
 };
