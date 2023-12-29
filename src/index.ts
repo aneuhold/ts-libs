a1424b55cec8009830b605db8bcbe79491962fc0
Anton G Neuhold Jr <agneuhold@gmail.com>
2023-12-28 18:05:15 -0800

    Fix spacing

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import BaseDocument from './documents/BaseDocument';
 import BaseDocumentWithType from './documents/BaseDocumentWithType';
-import ApiKey from './documents/common/ApiKey';
-import User from './documents/common/User';
-import DashboardUserConfig from './documents/dashboard/UserConfig';
+import ApiKey, { validateApiKey } from './documents/common/ApiKey';
+import User, { validateUser } from './documents/common/User';
+import DashboardUserConfig, {
+  validateDashboardUserConfig
+} from './documents/dashboard/UserConfig';
 import RequiredUserId from './schemas/required-refs/RequiredUserId';
 
 // Export all the functions and classes from this library
 export {
   User,
+  validateUser,
   ApiKey,
+  validateApiKey,
   DashboardUserConfig,
+  validateDashboardUserConfig,
   BaseDocument,
   BaseDocumentWithType,
   RequiredUserId
