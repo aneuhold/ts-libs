1ec7fd5bffd10683f68edb2187c400602e2572ec
Anton G Neuhold Jr <agneuhold@gmail.com>
2024-01-02 13:47:19 -0800

    Add insert functionality

--- src/services/DOFunctionService/functions/projectDashboard.ts
+++ src/services/DOFunctionService/functions/projectDashboard.ts
@@ -9,6 +9,9 @@ export interface ProjectDashboardOptions {
     userConfig?: boolean;
     tasks?: boolean;
   };
+  insert?: {
+    tasks?: DashboardTask[];
+  };
   update?: {
     userConfig?: DashboardUserConfig;
     tasks?: DashboardTask[];
