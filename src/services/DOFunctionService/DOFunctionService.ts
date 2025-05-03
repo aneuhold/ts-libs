ecb89e12f352d149e6a9d79c86858686278ccf7c
Anton G. Neuhold Jr. <agneuhold@gmail.com>
2025-05-02 20:28:44 -0700

    Update handleApiRequest for Tracing (#119)

--- src/services/DOFunctionService/DOFunctionService.ts
+++ src/services/DOFunctionService/DOFunctionService.ts
@@ -1,3 +1,4 @@
+import { DR } from '@aneuhold/core-ts-lib';
 import { BSON } from 'bson';
 import {
   DOFunctionCallOutput,
@@ -33,10 +34,12 @@ export default class DOFunctionService {
    * A generic method to handle any API request on the backend. This has
    * no use on the frontend.
    *
-   * This will take care of returning the error if the handler throws.
+   * This will take care of returning the error if the handler throws, and
+   * managing tracing spans via the registered ITracer.
    * Ideally the handler should not throw though unless something really
    * unexpected happened.
    *
+   * @param functionName - The name to use for the root tracing span.
    * @param rawInput - The raw input for the function.
    * @param handler - The handler function to process the input.
    * @returns The raw output of the function call.
@@ -45,39 +48,58 @@ export default class DOFunctionService {
     TInput extends DOFunctionInput,
     TOutput extends DOFunctionOutput
   >(
+    functionName: string,
     rawInput: DOFunctionRawInput,
     handler: (input: TInput) => Promise<DOFunctionCallOutput<TOutput>>
   ): Promise<DOFunctionRawOutput> {
-    // Default raw output
-    const rawOutput: DOFunctionRawOutput = {
-      body: '',
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/octet-stream'
+    return DR.tracer.startSpan(functionName, async (span) => {
+      // Default raw output
+      const rawOutput: DOFunctionRawOutput = {
+        body: '',
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/octet-stream'
+        }
+      };
+      // Default output
+      const defaultOutput: DOFunctionCallOutput<TOutput> = {
+        success: false,
+        errors: [],
+        data: {} as TOutput
+      };
+      try {
+        // Deserialize the input
+        const input: TInput = this.deserializeInput(rawInput);
+        // Call the handler
+        const output = await handler(input);
+        // Serialize the output
+        rawOutput.body = this.serializeOutput(output);
+        if (!output.success) {
+          rawOutput.statusCode = 400; // Bad Request if handler indicates failure
+          span?.setStatus({ code: 2, message: 'handler_error' }); // Indicate error in span
+        } else {
+          span?.setStatus({ code: 1, message: 'ok' }); // OK
+        }
+      } catch (e) {
+        // Capture unexpected errors
+        DR.tracer.captureException(e);
+        span?.setStatus({ code: 2, message: 'internal_error' }); // Internal Server Error
+
+        // Serialize as JSON if something fails to simplify in case the error
+        // happened in the normal serialization.
+        const error = e as Error;
+        defaultOutput.errors.push(JSON.stringify(error, null, 2));
+        rawOutput.body = JSON.stringify(defaultOutput);
+        rawOutput.headers['Content-Type'] = 'application/json';
+        rawOutput.statusCode = 500;
+      } finally {
+        // Ensure traces are flushed in serverless environments
+        if (DR.tracer.isEnabled()) {
+          await DR.tracer.flush(2000);
+        }
       }
-    };
-    // Default output
-    const defaultOutput: DOFunctionCallOutput<TOutput> = {
-      success: false,
-      errors: [],
-      data: {} as TOutput
-    };
-    try {
-      // Deserialize the input
-      const input: TInput = this.deserializeInput(rawInput);
-      // Call the handler
-      const output = await handler(input);
-      // Serialize the output
-      rawOutput.body = this.serializeOutput(output);
-    } catch (e) {
-      // Serialize as JSON if something fails to simplify in case the error
-      // happened in the normal serialization.
-      const error = e as Error;
-      defaultOutput.errors.push(JSON.stringify(error, null, 2));
-      rawOutput.body = JSON.stringify(defaultOutput);
-      rawOutput.headers['Content-Type'] = 'application/json';
-    }
-    return rawOutput;
+      return rawOutput;
+    });
   }
 
   /**
