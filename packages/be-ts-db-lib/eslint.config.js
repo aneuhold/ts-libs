b79d21209b765c906a00b442dadbcb9f71b34969
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-05-09 14:55:21 -0700

    Remove NX

--- packages/be-ts-db-lib/eslint.config.js
+++ packages/be-ts-db-lib/eslint.config.js
@@ -6,20 +6,6 @@ import baseConfig from '../../eslint.config.js';
 export default [
   ...baseConfig,
   {
-    files: ['**/*.json'],
-    rules: {
-      '@nx/dependency-checks': [
-        'error',
-        {
-          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}']
-        }
-      ]
-    },
-    languageOptions: {
-      parser: await import('jsonc-eslint-parser')
-    }
-  },
-  {
     // other override settings. e.g. for `files: ['**/*.test.*']`
     rules: {
       '@typescript-eslint/unbound-method': 'off'
