a32c96108c579d5c60d336ea427af40b0f7058a1
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-05-22 16:32:06 -0700

    Initial addition of local-npm-registry

---
+++ packages/local-npm-registry/eslint.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+import baseConfig from '../../eslint.config.js';
+
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray} */
+export default [
+  ...baseConfig,
+  {
+    // other override settings. e.g. for `files: ['**/*.test.*']`
+    rules: {}
+  }
+];
