7cfd46321ba26fdf70cf0e4420e65a94c58d48a2
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-10-16 17:13:16 -0700

    Fix docs

--- packages/core-ts-lib/src/types/PackageJsonInfo.ts
+++ packages/core-ts-lib/src/types/PackageJsonInfo.ts
@@ -3,9 +3,6 @@ import { PackageJson } from './PackageJson.js';
 /**
  * Contains the parsed contents and file path of a package.json file.
  * Used for package metadata management.
- *
- * @param packageJsonContents The parsed package.json object
- * @param packageJsonPath The absolute path to the package.json file
  */
 export interface PackageJsonInfo {
   packageJsonContents: PackageJson;
