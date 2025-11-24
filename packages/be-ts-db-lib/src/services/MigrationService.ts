6f71aba171da52ba445e961ae0ef555de29c52a7
Anton G Neuhold Jr <agneuhold@gmail.com>
2025-11-23 17:19:13 -0800

    Cleanup and simplify

--- packages/be-ts-db-lib/src/services/MigrationService.ts
+++ packages/be-ts-db-lib/src/services/MigrationService.ts
@@ -1,7 +1,16 @@
 /* eslint-disable */
 // @ts-nocheck
+import {
+  ApiKey,
+  DashboardTask,
+  DashboardUserConfig,
+  DocumentService,
+  NonogramKatanaItem,
+  NonogramKatanaUpgrade,
+  User
+} from '@aneuhold/core-ts-db-lib';
 import { DR } from '@aneuhold/core-ts-lib';
-import type { User } from 'packages/core-ts-db-lib/lib/browser.js';
+import { v7 as uuidv7 } from 'uuid';
 import ApiKeyRepository from '../repositories/common/ApiKeyRepository.js';
 import UserRepository from '../repositories/common/UserRepository.js';
 import DashboardNonogramKatanaItemRepository from '../repositories/dashboard/DashboardNonogramKatanaItemRepository.js';
@@ -44,8 +53,10 @@ export default class MigrationService {
     const nonogramItems = await nonogramItemRepo.getAll();
     const nonogramUpgrades = await nonogramUpgradesRepo.getAll();
 
-    DR.logger.info(`Loaded ${users.length} users, ${apiKeys.length} API keys, ${tasks.length} tasks, ${configs.length} configs, ${nonogramItems.length} nonogram items, and ${nonogramUpgrades.length} nonogram upgrades.`);
-    
+    DR.logger.info(
+      `Loaded ${users.length} users, ${apiKeys.length} API keys, ${tasks.length} tasks, ${configs.length} configs, ${nonogramItems.length} nonogram items, and ${nonogramUpgrades.length} nonogram upgrades.`
+    );
+
     const countObjectIds = (docs: any[]) => {
       docs.forEach((doc) => {
         // Get the type of the _id field
@@ -54,30 +65,169 @@ export default class MigrationService {
     };
 
     // Create a map of document IDs to new UUIDs
-    const allDocs = [...users, ...apiKeys, ...tasks, ...configs, ...nonogramItems, ...nonogramUpgrades];
-    const legacyDocs = allDocs.filter(doc => typeof doc._id === 'object');
-    const newDocs = allDocs.filter(doc => typeof doc._id === 'string');
+    const allDocs = [
+      ...users,
+      ...apiKeys,
+      ...tasks,
+      ...configs,
+      ...nonogramItems,
+      ...nonogramUpgrades
+    ];
+    const legacyDocs = allDocs.filter((doc) => typeof doc._id === 'object');
+    const newDocs = allDocs.filter((doc) => typeof doc._id === 'string');
 
-    DR.logger.info(`Found ${legacyDocs.length} documents with legacy ObjectId IDs and ${newDocs.length} documents with string IDs.`);
+    DR.logger.info(
+      `Found ${legacyDocs.length} documents with legacy ObjectId IDs and ${newDocs.length} documents with string IDs.`
+    );
 
     // Now create the actual map
     const mapFromObjectIdToUUID = new Map<string, string>();
-    newDocs.forEach(doc => {
+    newDocs.forEach((doc) => {
       if (!doc.oldOId) {
         DR.logger.warn(`Document with string ID ${doc._id} is missing oldOId field.`);
+        return;
       }
       mapFromObjectIdToUUID.set(doc.oldOId, doc._id);
     });
     DR.logger.info(`Created mapping for ${mapFromObjectIdToUUID.size} documents.`);
 
-    // Create the function that will flesh out new documents for a given user document
     const newUsersToCreate: User[] = [];
+    const newApiKeysToCreate: ApiKey[] = [];
+    const newTasksToCreate: DashboardTask[] = [];
+    const newConfigsToCreate: DashboardUserConfig[] = [];
+    const newNonogramItemsToCreate: NonogramKatanaItem[] = [];
+    const newNonogramUpgradesToCreate: NonogramKatanaUpgrade[] = [];
+
+    /**
+     * Gets a UUID for a given legacy ObjectId. If one already exists in the map,
+     * it is returned. Otherwise, a new one is generated and added to the map.
+     */
+    const getUUID = (oldId: ObjectId): string => {
+      const oldIdStr = oldId.toString();
+      if (mapFromObjectIdToUUID.has(oldIdStr)) {
+        return mapFromObjectIdToUUID.get(oldIdStr);
+      }
+      const newId = uuidv7();
+      mapFromObjectIdToUUID.set(oldIdStr, newId);
+      return newId;
+    };
+
+    // Create the function that will flesh out new documents for a given user document
     function createNewDocsForUser(oldUserDoc: User) {
-      if (mapFromObjectIdToUUID.has(oldUserDoc._id.toString())) {
-        DR.logger.warn(`User document with ObjectId ID ${oldUserDoc._id} already has a mapped UUID. Skipping creation.`);
+      const oldUserIdStr = oldUserDoc._id.toString();
+      if (mapFromObjectIdToUUID.has(oldUserIdStr)) {
+        DR.logger.warn(
+          `User document with ObjectId ID ${oldUserDoc._id} already has a mapped UUID in the DB. Skipping creation.`
+        );
+        // I might want to create these anyway at some point. Still deciding.
         return;
       }
+
+      function createNewDoc(oldDoc) {
+        if (mapFromObjectIdToUUID.has(oldDoc._id.toString())) return;
+        const newDocId = getUUID(oldDoc._id);
+        const newDoc = DocumentService.deepCopy(oldDoc);
+        newDoc._id = newDocId;
+        newDoc.oldOId = oldDoc._id.toString();
+        return newDoc;
+      }
+
+      // 1. Create the new user document
+      const newUser = createNewDoc(oldUserDoc);
+      if (newUser) {
+        newUsersToCreate.push(newUser);
+      }
+     
+      // 2. Create new API keys
+      const userApiKeys = apiKeys.filter((k) => k.userId.toString() === oldUserIdStr);
+      userApiKeys.forEach((oldKey) => {
+        const newKey = createNewDoc(oldKey);
+        if (!newKey) return;
+        newKey.userId = newUser._id;
+        newApiKeysToCreate.push(newKey);
+      });
+
+      // 3. Create new Tasks
+      const userTasks = tasks.filter((t) => t.userId.toString() === oldUserIdStr);
+      userTasks.forEach((oldTask) => {
+        const newTask = createNewDoc(oldTask);
+        if (!newTask) return;
+        newTask.userId = newUserId;
+
+        // Map user IDs in tags keys
+        const newTags: any = {};
+        Object.keys(oldTask.tags).forEach((tagUserId) => {
+          const newTagUserId = getUUID(tagUserId);
+          newTags[newTagUserId] = oldTask.tags[tagUserId];
+        });
+        newTask.tags = newTags;
+
+        // Map user IDs in filterSettings keys
+        const newFilterSettings: any = {};
+        Object.keys(oldTask.filterSettings).forEach((fsUserId) => {
+          const newFsUserId = getUUID(fsUserId);
+          newFilterSettings[newFsUserId] = oldTask.filterSettings[fsUserId];
+        });
+        newTask.filterSettings = newFilterSettings;
+
+        // Map user IDs in sortSettings keys
+        const newSortSettings: any = {};
+        Object.keys(oldTask.sortSettings).forEach((ssUserId) => {
+          const newSsUserId = getUUID(ssUserId);
+          newSortSettings[newSsUserId] = oldTask.sortSettings[ssUserId];
+        });
+        newTask.sortSettings = newSortSettings;
+
+        newTask.sharedWith = oldTask.sharedWith.map((id: any) => getUUID(id));
+        if (oldTask.assignedTo) {
+          newTask.assignedTo = getUUID(oldTask.assignedTo);
+        }
+        if (oldTask.parentTaskId) {
+          newTask.parentTaskId = getUUID(oldTask.parentTaskId);
+        }
+
+        newTasksToCreate.push(newTask);
+      });
+
+      // 4. Create new Configs
+      const userConfigs = configs.filter((c) => c.userId.toString() === oldUserIdStr);
+      userConfigs.forEach((oldConfig) => {
+        const newConfig = createNewDoc(oldConfig);
+        if (!newConfig) return;
+        newConfig.userId = newUserId;
+        newConfig.collaborators = oldConfig.collaborators.map((id: any) => getUUID(id));
+        newConfigsToCreate.push(newConfig);
+      });
+
+      // 5. Nonogram Items
+      const userNonogramItems = nonogramItems.filter((i) => i.userId.toString() === oldUserIdStr);
+      userNonogramItems.forEach((oldItem) => {
+        const newItem = createNewDoc(oldItem);
+        if (!newItem) return;
+        newItem.userId = newUserId;
+        newNonogramItemsToCreate.push(newItem);
+      });
+
+      // 6. Nonogram Upgrades
+      const userNonogramUpgrades = nonogramUpgrades.filter((u) => u.userId.toString() === oldUserIdStr);
+      userNonogramUpgrades.forEach((oldUpgrade) => {
+        const newUpgrade = createNewDoc(oldUpgrade);
+        if (!newUpgrade) return;
+        newUpgrade.userId = newUserId;
+        newNonogramUpgradesToCreate.push(newUpgrade);
+      });
     }
+
+    // Run the creation function for a single user to test
+    const legacyUsers = users.filter((u) => u.userName === 'demoUser1');
+    legacyUsers.forEach((u) => createNewDocsForUser(u));
+
+    DR.logger.info(`Created ${newUsersToCreate.length} new users.`);
+    DR.logger.info(`Created ${newApiKeysToCreate.length} new API keys.`);
+    DR.logger.info(`Created ${newTasksToCreate.length} new tasks.`);
+    DR.logger.info(`Created ${newConfigsToCreate.length} new configs.`);
+    DR.logger.info(`Created ${newNonogramItemsToCreate.length} new nonogram items.`);
+    DR.logger.info(`Created ${newNonogramUpgradesToCreate.length} new nonogram upgrades.`);
 
     return;
   }
